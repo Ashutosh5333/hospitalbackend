5e76193243655de839f753bc36ed125d29ebf247
Ashutosh5333 <lakshakarashutosh@gmail.com>
2023-03-22 12:13:45 +0530

    delete and updted done

--- Routes/Docter.js
+++ Routes/Docter.js
@@ -21,6 +21,27 @@ const { DoctorModel } = require("../models/Doctors.model")
            res.send("userdata Created successfully")
     })
 
+    DoctorRouter.patch("/doctor/:Id" ,async(req,res) =>{
+            const Id = req.params.Id
+            const payload= req.body;
+            try{
+                   await DoctorModel.findByIdAndUpdate({_id:Id},payload)
+                res.send("user data updated sucessfully")
+            }
+            catch(err){
+              res.send("somethind went wrong")
+            } 
+    })
+    DoctorRouter.delete("/doctor/:Id" ,async(req,res) =>{
+      const Id = req.params.Id
+      try{
+             await DoctorModel.findByIdAndDelete({_id:Id})
+          res.send("doctor data deleted sucessfully")
+      }
+      catch(err){
+        res.send("somethind went wrong")
+      } 
+})
 
    module.exports={
      DoctorRouter
